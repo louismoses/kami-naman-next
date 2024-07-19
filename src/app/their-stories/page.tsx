352ab44aba93e3f60f8dc5e35c2e264653b960a0
Louis Moses <louismoses.rait@gmail.com>
2024-07-19 20:44:15 +0800

    initial commit for shadcn

---
+++ src/app/their-stories/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+export default function TheirStories() {
+  return (
+    <div>
+      <h1 className="text-5xl">Their Stories</h1>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+}
