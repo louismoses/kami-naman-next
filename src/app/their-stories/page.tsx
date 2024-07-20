6a4fc71d09201603601ad4cc099d2315ab344512
Louis Moses <louismoses.rait@gmail.com>
2024-07-20 17:42:17 +0800

    test: static html output

--- src/app/their-stories/page.tsx
+++ src/app/their-stories/page.tsx
@@ -5,7 +5,7 @@ export default function TheirStories() {
   return (
     <div>
       <h1 className="text-5xl">Their Stories</h1>
-      <Button variant="outline">Button</Button>
+      <Button variant="outlined">Button</Button>
     </div>
   );
 }
