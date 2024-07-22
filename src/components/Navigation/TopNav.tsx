6b2f45e47160341d0ddd723428dc296bf42eb6c9
Louis Moses <louismoses.rait@gmail.com>
2024-07-22 20:58:38 +0800

    responsive: add hamburger menu

--- src/components/Navigation/TopNav.tsx
+++ src/components/Navigation/TopNav.tsx
@@ -3,6 +3,16 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { IoMenu } from "react-icons/io5";
 
 interface MenuItem {
   title: string;
@@ -44,7 +54,7 @@ const TopNav = ({}) => {
           priority
         />
       </Link>
-      <div className="flex gap-7 text-white items-center">
+      <div className="hidden lg:flex gap-7 text-white items-center ">
         {menus.map((menu, index) => (
           <Link
             href={menu.path}
@@ -59,6 +69,39 @@ const TopNav = ({}) => {
         <Link href={"/sign-petition"}>
           <Button variant={"knYellow"}>Sign the Petition</Button>
         </Link>
+      </div>
+      {/* sheet */}
+      <div className="block lg:hidden ">
+        <Sheet>
+          <SheetTrigger>
+            <IoMenu color="white" />
+          </SheetTrigger>
+          <SheetContent side={"right"}>
+            <SheetHeader>
+              {/* <SheetTitle>Menu</SheetTitle> */}
+              <SheetDescription>
+                <div className="flex flex-col gap-5  pt-4">
+                  {menus.map((menu, index) => (
+                    <Link
+                      href={menu.path}
+                      key={index}
+                      className={`font-semibold text-sm border-b pb-5  ${
+                        menu.path === pathname
+                          ? " underline kn-text-yellow"
+                          : ""
+                      }`}
+                    >
+                      <span className="">{menu.title}</span>
+                    </Link>
+                  ))}
+                  <Link href={"/sign-petition"}>
+                    <Button variant={"knYellow"}>Sign the Petition</Button>
+                  </Link>
+                </div>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
   );
