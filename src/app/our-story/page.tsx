9a44e2db95db6eff79aaebc46464abf2001993a5
Louis Moses <louismoses.rait@gmail.com>
2024-07-24 19:00:01 +0800

    feat(page): add our story page content

--- src/app/our-story/page.tsx
+++ src/app/our-story/page.tsx
@@ -20,6 +20,38 @@ const OurStory = ({}) => {
           a positive change.
         </p>
       </div>
+      <div className="grid lg:grid-cols-2 gap-20  my-20">
+        <div>
+          <h2 className="text-2xl kn-text-yellow font-black drop-shadow-lg">
+            MISSION
+          </h2>
+          <p className=" pt-4">
+            Raise awareness and empower youth living inside the residential
+            shelters for both NGO and governmental to have support and guidance
+            through the Independent Living Program, nurturing and encouraging
+            the youth.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-2xl kn-text-yellow font-black drop-shadow-lg">
+            VISION
+          </h2>
+          <p className=" pt-4">
+            Be a significant movement and one of the supports for the lives of
+            the youth who are living inside the residential shelters, supporting
+            the Independent Living Program nationwide.
+          </p>
+        </div>
+        <div className="font-bold">
+          <h2 className="text-2xl kn-text-yellow font-black drop-shadow-lg">
+            PROPONENTS
+          </h2>
+          <p className=" pt-4">Cruz, Kyle Gabriel</p>
+          <p>Dizon, Catherine Alessandra</p>
+          <p>Espero, Redson Tranx </p>
+          <p>Ignacio, Kiziah Hanaiel</p>
+        </div>
+      </div>
     </div>
   );
 };
